e75a1f72c3b8a3c93c25f3c5096f681dd102006e
Chris <stac1221@gmail.com>
2019-10-08 22:30:05 -0500

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="layout">
+    <header class="header">
+      <div class="container">
+        <strong>
+          <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+        </strong>
+        <nav class="nav">
+          <g-link class="nav__link" to="/">Home</g-link>
+          <g-link class="nav__link" to="/about/">About</g-link>
+        </nav>
+      </div>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+  @import '../styles/main';
+</style>
